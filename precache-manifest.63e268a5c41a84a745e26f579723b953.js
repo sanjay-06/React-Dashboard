00ad4153f247a292967d1f08dd5e30549901f443
sanjay <san692001@gmail.com>
2020-12-26 17:44:26 +0530

    Updates

---
+++ precache-manifest.63e268a5c41a84a745e26f579723b953.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3985671c4f525706c2a115043873299",
+    "url": "/React-Dashboard/index.html"
+  },
+  {
+    "revision": "5a1d82204be5ac5c13e8",
+    "url": "/React-Dashboard/static/css/2.b47d4936.chunk.css"
+  },
+  {
+    "revision": "08e80d98f06643d6e801",
+    "url": "/React-Dashboard/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "5a1d82204be5ac5c13e8",
+    "url": "/React-Dashboard/static/js/2.ecc05eaf.chunk.js"
+  },
+  {
+    "revision": "e4ef2bbe1ac63b295e2d89417f7e2378",
+    "url": "/React-Dashboard/static/js/2.ecc05eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e80d98f06643d6e801",
+    "url": "/React-Dashboard/static/js/main.9ef9a984.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/React-Dashboard/static/js/main.9ef9a984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a489b51c088eca9c2e4",
+    "url": "/React-Dashboard/static/js/runtime-main.f340d582.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/React-Dashboard/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "526a4d97e904d13d5d9afb648ee30b4f",
+    "url": "/React-Dashboard/static/media/sidebar-2.526a4d97.jpg"
+  }
+]);
